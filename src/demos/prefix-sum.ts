ff01903be4626283c6668002a9a27449e908bdd4
Mikola Lysenko <mikolalysenko@gmail.com>
2023-02-12 12:55:34 -0500

    refactor prefix sum into separate library

--- src/demos/prefix-sum.ts
+++ src/demos/prefix-sum.ts
@@ -1,214 +1,20 @@
 import { makeBench, mustHave } from '../boilerplate'
-
-const THREADS_PER_GROUP_X = 256
-const THREADS_PER_GROUP_Y = 1
-const THREADS_PER_GROUP_Z = 1
-const THREADS_PER_GROUP = THREADS_PER_GROUP_X * THREADS_PER_GROUP_Y * THREADS_PER_GROUP_Z
-const ITEMS_PER_THREAD = 256
-const ITEMS_PER_GROUP = ITEMS_PER_THREAD * THREADS_PER_GROUP
-
-// used bank conflict resolution, does not seem to make any difference on rtx4090
-const LOG_NUM_BANKS = 5
+import { WebGPUScan } from '../lib/scan'
 
 async function main () {
   const adapter = mustHave(await navigator.gpu.requestAdapter())
   const device = await adapter.requestDevice()
 
-  const prefixSumShader = device.createShaderModule({
-    code: `
-struct SumParams {
-  N : u32,
-}
-@binding(0) @group(0) var<uniform> params : SumParams;
-@binding(1) @group(0) var<storage, read_write> post : array<f32>;
-@binding(2) @group(0) var<storage, read_write> data : array<f32>;
-@binding(3) @group(0) var<storage, read_write> work : array<f32>;
-
-fn conflictFreeOffset (offset:u32) -> u32 {
-  return offset + (offset >> ${LOG_NUM_BANKS});
-}
-
-var<workgroup> workerSums : array<f32, ${2 * THREADS_PER_GROUP}>;
-fn partialSum (localId : u32) -> f32 {
-  var offset = 1u;
-  for (var d = ${THREADS_PER_GROUP >> 1}u; d > 0u; d = d >> 1u) {
-    if (localId < d) {
-      var ai = conflictFreeOffset(offset * (2u * localId + 1u) - 1u);
-      var bi = conflictFreeOffset(offset * (2u * localId + 2u) - 1u);
-      workerSums[bi] = workerSums[bi] + workerSums[ai];
-    }
-    offset *= 2u;
-    workgroupBarrier();
-  }
-
-  if (localId == 0u) {
-    workerSums[conflictFreeOffset(${THREADS_PER_GROUP - 1}u)] = 0.;
-  }
-
-  for (var d = 1u; d < ${THREADS_PER_GROUP}u; d = d * 2u) {
-    offset = offset >> 1u;
-    if (localId < d) {
-      var ai = conflictFreeOffset(offset * (2u * localId + 1u) - 1u);
-      var bi = conflictFreeOffset(offset * (2u * localId + 2u) - 1u);
-      var a = workerSums[ai];
-      var b = workerSums[bi];
-      workerSums[ai] = b;
-      workerSums[bi] = a + b;
-    }
-    workgroupBarrier();
-  }
-
-  return workerSums[conflictFreeOffset(localId)];
-}
-
-@compute @workgroup_size(${THREADS_PER_GROUP_X}, ${THREADS_PER_GROUP_Y}, ${THREADS_PER_GROUP_Z})
-fn prefixSumIn(
-  @builtin(workgroup_id) groupId : vec3<u32>,
-  @builtin(local_invocation_id) localVec : vec3<u32>,
-  @builtin(global_invocation_id) globalVec : vec3<u32>) {
-  var localId = ${THREADS_PER_GROUP_Y * THREADS_PER_GROUP_Z}u * localVec.x + ${THREADS_PER_GROUP_Z}u * localVec.y + localVec.z;
-  var globalId = ${THREADS_PER_GROUP_Y * THREADS_PER_GROUP_Z}u * globalVec.x + ${THREADS_PER_GROUP_Z}u * globalVec.y + globalVec.z;
-
-  var N = params.N;
-
-  var s = 0.;
-  var localVals = array<f32, ${ITEMS_PER_THREAD}>();
-  for (var i = 0u; i < ${ITEMS_PER_THREAD}; i = i + 1u) {
-    s = s + data[${ITEMS_PER_THREAD} * globalId + i];
-    localVals[i] = s;
-  }
-  workerSums[conflictFreeOffset(localId)] = s;
-  workgroupBarrier();
-
-  s = partialSum(localId);
-
-  for (var i = 0u; i < ${ITEMS_PER_THREAD}; i = i + 1u) {
-    work[${ITEMS_PER_THREAD} * globalId + i] = s + localVals[i];
-  }
-  if (localId == ${THREADS_PER_GROUP - 1}u) {
-    post[groupId.x] = s + localVals[${ITEMS_PER_THREAD} - 1];
-  }
-}
-
-@compute @workgroup_size(${THREADS_PER_GROUP_X}, ${THREADS_PER_GROUP_Y}, ${THREADS_PER_GROUP_Z})
-fn prefixSumPost(@builtin(local_invocation_id) localVec : vec3<u32>) {
-  var localId = ${THREADS_PER_GROUP_Y * THREADS_PER_GROUP_Z}u * localVec.x + ${THREADS_PER_GROUP_Z}u * localVec.y + localVec.z;
-
-  var s = 0.;
-  var localVals = array<f32, ${ITEMS_PER_THREAD}>();
-  for (var i = 0u; i < ${ITEMS_PER_THREAD}; i = i + 1u) {
-    s = s + post[${ITEMS_PER_THREAD} * localId + i];
-    localVals[i] = s;
-  }
-  workerSums[conflictFreeOffset(localId)] = s;
-  workgroupBarrier();
-
-  s = partialSum(localId);
-  for (var i = 0u; i < ${ITEMS_PER_THREAD}; i = i + 1u) {
-    post[${ITEMS_PER_THREAD} * localId + i] = s + localVals[i];
-  }
-}
-
-@compute @workgroup_size(${THREADS_PER_GROUP_X}, ${THREADS_PER_GROUP_Y}, ${THREADS_PER_GROUP_Z})
-fn prefixSumOut(
-  @builtin(workgroup_id) groupId : vec3<u32>,
-  @builtin(global_invocation_id) globalVec : vec3<u32>) {
-  var globalId = globalVec.x * ${THREADS_PER_GROUP_Y * THREADS_PER_GROUP_Z}u + globalVec.y * ${THREADS_PER_GROUP_Z}u + globalVec.z;
-  if (groupId.x > 0u) {
-    var s = post[groupId.x - 1u];
-    for (var i = 0u; i < ${ITEMS_PER_THREAD}; i = i + 1u) {
-      data[${ITEMS_PER_THREAD} * globalId + i] = s + work[${ITEMS_PER_THREAD} * globalId + i];
-    }
-  } else {
-    for (var i = 0u; i < ${ITEMS_PER_THREAD}; i = i + 1u) {
-      data[${ITEMS_PER_THREAD} * globalId + i] = work[${ITEMS_PER_THREAD} * globalId + i];
-    }
-  }
-}
-`
+  const prefixSum = new WebGPUScan({
+    device
   })
-
-  const layout = device.createPipelineLayout({
-    bindGroupLayouts: [
-      device.createBindGroupLayout({
-        entries: [{
-          binding: 0,
-          visibility: GPUShaderStage.COMPUTE,
-          buffer: {
-            type: 'uniform'
-          }
-        }, {
-          binding: 1,
-          visibility: GPUShaderStage.COMPUTE,
-          buffer: {
-            type: 'storage'
-          }
-        }, {
-          binding: 2,
-          visibility: GPUShaderStage.COMPUTE,
-          buffer: {
-            type: 'storage'
-          }
-        }, {
-          binding: 3,
-          visibility: GPUShaderStage.COMPUTE,
-          buffer: {
-            type: 'storage'
-          }
-        }]
-      } as const),
-    ]
-  })
-
-  const prefixSumIn = device.createComputePipeline({
-    layout,
-    compute: {
-      module: prefixSumShader,
-      entryPoint: 'prefixSumIn'
-    }
-  })
-
-  const prefixSumPost = device.createComputePipeline({
-    layout,
-    compute: {
-      module: prefixSumShader,
-      entryPoint: 'prefixSumPost'
-    }
-  })
-
-  const prefixSumOut = device.createComputePipeline({
-    layout,
-    compute: {
-      module: prefixSumShader,
-      entryPoint: 'prefixSumOut'
-    }
-  })
-
 
   const kernels = {
     async gpu(n:number) {
-      const paramBuffer = device.createBuffer({
-        size: 1 * 4,
-        usage: GPUBufferUsage.UNIFORM,
-        mappedAtCreation: true
-      })
-      new Uint32Array(paramBuffer.getMappedRange()).set([ n ])
-      paramBuffer.unmap()
-
-      const postBuffer = device.createBuffer({
-        label: 'postBuffer',
-        size: ITEMS_PER_GROUP * 4,
-        usage: GPUBufferUsage.STORAGE
-      })
       const dataBuffer = device.createBuffer({
         label: 'dataBuffer',
         size: n * 4,
         usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST | GPUBufferUsage.COPY_SRC
-      })
-      const workBuffer = device.createBuffer({
-        label: 'workBuffer',
-        size: n * 4,
-        usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC 
       })
       const readBuffer = device.createBuffer({
         label: 'readBuffer',
@@ -216,30 +22,7 @@ fn prefixSumOut(
         usage: GPUBufferUsage.MAP_READ | GPUBufferUsage.COPY_DST
       })
 
-      const prefixSumBindGroup = device.createBindGroup({
-        layout: prefixSumIn.getBindGroupLayout(0),
-        entries: [{
-          binding: 0,
-          resource: {
-            buffer: paramBuffer
-          }
-        }, {
-          binding: 1,
-          resource: {
-            buffer: postBuffer
-          }
-        }, {
-          binding: 2,
-          resource: {
-            buffer: dataBuffer
-          }
-        }, {
-          binding: 3,
-          resource: {
-            buffer: workBuffer
-          }
-        }]
-      })
+      const pass = await prefixSum.createPass(n, dataBuffer)
 
       return {
         async prefixsum (out:Float32Array, src:Float32Array, skipTransfer:boolean) {
@@ -250,26 +33,11 @@ fn prefixSumOut(
           const commandEncoder = device.createCommandEncoder()
           const passEncoder = commandEncoder.beginComputePass()
 
-          // set up bind groups
-          passEncoder.setBindGroup(0, prefixSumBindGroup)
-          
-          passEncoder.setPipeline(prefixSumIn)
-          passEncoder.dispatchWorkgroups(n / ITEMS_PER_GROUP, 1, 1)
-          if (n > ITEMS_PER_GROUP) {
-            passEncoder.setPipeline(prefixSumPost)
-            passEncoder.dispatchWorkgroups(1, 1, 1)
-            passEncoder.setPipeline(prefixSumOut)
-            passEncoder.dispatchWorkgroups(n / ITEMS_PER_GROUP, 1, 1)
-          }
-
+          pass.run(passEncoder)
           passEncoder.end()
 
           if (!skipTransfer) {
-            if (n > ITEMS_PER_GROUP) {
-              commandEncoder.copyBufferToBuffer(dataBuffer, 0, readBuffer, 0, 4 * n)
-            } else {
-              commandEncoder.copyBufferToBuffer(workBuffer, 0, readBuffer, 0, 4 * n)
-            }
+            commandEncoder.copyBufferToBuffer(dataBuffer, 0, readBuffer, 0, 4 * n)
           }
 
           device.queue.submit([commandEncoder.finish()])
@@ -283,10 +51,9 @@ fn prefixSumOut(
           }
         },
         async free () {
-          paramBuffer.destroy()
+          pass.destroy();
           dataBuffer.destroy()
           readBuffer.destroy()
-          postBuffer.destroy()
         }
       }
     },
@@ -304,8 +71,8 @@ fn prefixSumOut(
     },
   } as const
 
-  const minN = Math.log2(ITEMS_PER_GROUP)
-  const maxN = Math.min(25, 2 * minN)
+  const minN = Math.log2(prefixSum.minItems()) | 0
+  const maxN = Math.log2(prefixSum.maxItems()) | 0
   const rangeN = {
     type: 'number',
     min: '' + minN,
@@ -314,7 +81,7 @@ fn prefixSumOut(
   }
 
   const ui = makeBench({
-    header: `A pretty slow and clumsy GPU prefix sum.  Array length N must be a multiple of ${ITEMS_PER_GROUP}.
+    header: `GPU prefix sum/scan demo.  Array length N must be a multiple of ${prefixSum.itemsPerGroup}.
 GPU performance is bottlenecked by data transfer costs.  Disabling CPU transfer will improve performance.`,
     inputs: {
       startN:{
